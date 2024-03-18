4801aa3f2cb28f564a08e1c8b6e0606a1078a6a2
Armin Salihović <armin.salihovic@live.com>
2024-03-18 22:33:18 +0100

    Change color of "Remove" button

--- resources/js/Components/Button.vue
+++ resources/js/Components/Button.vue
@@ -4,11 +4,22 @@ defineProps({
         type: String,
         default: 'submit',
     },
+    color: {
+        type: String,
+        default: 'gray',
+    },
 });
+
+const colors = {
+    gray: 'text-white bg-gray-800 focus:border-gray-900 focus:shadow-outline-gray hover:bg-gray-700 active:bg-gray-900',
+    orange: 'text-white bg-orange-800 focus:border-orange-900 focus:shadow-outline-orange hover:bg-orange-700 active:bg-orange-900'
+}
 </script>
 
 <template>
-    <button :type="type" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray transition ease-in-out duration-150">
+<!--    text-white bg-gray-800 focus:border-gray-900 focus:shadow-outline-gray hover:bg-gray-700 active:bg-gray-900-->
+<!--    text-white bg-orange-800 focus:border-orange-900 focus:shadow-outline-orange hover:bg-orange-700 active:bg-orange-900-->
+    <button :type="type" :class="colors[color] + ' inline-flex items-center px-4 py-2 border border-transparent rounded-md font-semibold text-xs uppercase tracking-widest focus:outline-none transition ease-in-out duration-150'">
         <slot />
     </button>
 </template>
